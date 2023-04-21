6113c790c00551d383c8d6e127e39c394e7c839f
AdelBeltran <adelbeltran03@gmail.com>
2023-04-21 11:49:56 -0400

    Delete

---
+++ account_entry_posted_draft_delete/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': "Delete Account Journal Posted Entry",
+    'version' : '14.1.1.1',
+    'summary': 'Using this module you can Delete Account Journal Posted Entry in any stage of record from for view and list view, user wise.',
+    'category': 'Accounting',
+    'description': """ Using this module you can Delete Account Journal Posted Entry in any stage of record from for view and list view, user wise. """,
+    "price": 10,
+    'currency': 'EUR',
+    "author" : "MAISOLUTIONSLLC",
+    'sequence': 1,
+    "email": 'apps@maisolutionsllc.com',
+    "website":'http://maisolutionsllc.com/',
+    'license': 'OPL-1',
+    "depends" : ['base','account'],
+	"data": [
+		'security/account.xml',
+		'views/account_view.xml',
+	],
+    'images': [],
+    'qweb': [],
+    'images': ['static/description/main_screenshot.png'],
+    "live_test_url" : "https://youtu.be/c74wF9ctWIo ",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
